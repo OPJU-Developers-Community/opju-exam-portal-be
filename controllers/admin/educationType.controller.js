4ecf5dc9fcdca0ecbcc7f662e7e46c6610ac5f0e
Tanusingh02 <tanu021202@gmail.com>
2023-09-16 01:00:07 +0530

    minor changes

--- controllers/admin/educationType.controller.js
+++ controllers/admin/educationType.controller.js
@@ -3,7 +3,7 @@ const Option = require("../../models/admin/educationType.model");
 async function getOptions(req, res) {
   try {
     const data = await Option.find({})
-    res.status(200).json({ message: "success", data });
+    res.status(200).json({ success: true, message: "success", data });
   } 
   catch (error) {
     return res.status(500).json({message: "Internal Server Error!"});
