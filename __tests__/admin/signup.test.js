904422783ae9fe4cb35d64a2f61cac331b453786
Subrat kumar jena <subratj611@gmail.com>
2023-11-07 21:57:10 +0530

    test: sign up test suites add

--- __tests__/admin/signup.test.js
+++ __tests__/admin/signup.test.js
@@ -1,17 +1,23 @@
 const request = require("supertest");
 const app = require("../../app");
+const { AdminAuth } = require("../../models/admin/auth.model");
 
 describe("Signup route", () => {
-  const testResponse = {
-    data: {
-      email: "admin@yopmail.com",
-      username: "admin",
-    },
-    message: "Signup successfully",
-    success: true,
-  };
+  beforeEach(async () => {
+    // Clear the admin auth collection in the database to ensure a clean state before each test
+    await AdminAuth.deleteMany({});
+  });
 
-  test("should sign up a user with valid input", async () => {
+  test("should sign up a user - 201 OK", async () => {
+    const testData = {
+      data: {
+        email: "admin@yopmail.com",
+        username: "admin",
+      },
+      message: "Signup successfully",
+      success: true,
+    };
+
     const response = await request(app)
       .post("/api/admin/signup")
       .send({
@@ -22,12 +28,39 @@ describe("Signup route", () => {
       .expect("Content-Type", /json/);
 
     const expectedResponse = {
-      data: expect.objectContaining(testResponse.data),
-      message: testResponse.message,
-      success: testResponse.success,
+      data: expect.objectContaining(testData.data),
+      message: testData.message,
+      success: testData.success,
     };
 
     expect(response.statusCode).toBe(201);
     expect(response.body).toEqual(expectedResponse);
+  });
+
+  test("should not sign up a user if user exist already - 401", async () => {
+    const testData = {
+      username: "admin",
+      email: "admin@yopmail.com",
+      password: "admin@123",
+    };
+
+    await AdminAuth.create(testData);
+
+    const response = await request(app)
+      .post("/api/admin/signup")
+      .send({
+        username: "admin",
+        email: "admin@yopmail.com",
+        password: "admin@123",
+      })
+      .expect("Content-Type", /json/);
+
+    const expectedResponse = {
+      data: expect.objectContaining(testData.data),
+      message: testData.message,
+      success: testData.success,
+    };
+
+    expect(response.statusCode).toBe(401);
   });
 });
