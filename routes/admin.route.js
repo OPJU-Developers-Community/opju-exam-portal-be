070d1bb6bf14146795e1a8d0762178c7606a2a43
Subrat kumar jena <subratj611@gmail.com>
2023-11-12 21:28:41 +0530

    refactor: add comment in code, refactor routes and model

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -9,8 +9,9 @@ const {
 } = require("../controllers/admin/usermanagement.controller");
 const optionsController = require("../controllers/admin/educationType.controller");
 const {
-  createEducation, getEducation,
-} = require("../controllers/admin/educationmanagement.controller");
+  createEducation,
+  getEducation,
+} = require("../controllers/admin/education.controller");
 const getOptions = require("../controllers/admin/educationType.controller");
 
 const router = express.Router();
@@ -24,12 +25,38 @@ router.post("/login", adminLogin);
 // {DEV_DOMAIN}:{PORT}/api/admin/user-management
 router.route("/user-management").post(addUser).get(getUser);
 
-// {DEV_DOMAIN}:{PORT}/api/admin/education-management
-router
-  .route("/education-management")
-  .post(createEducation)
-  .get(getEducation);
 
-router.get("/education-type", getOptions);
+/**
+ * @description Create an education for univeristy or school
+ * @route POST /create-education
+ * @access Private
+ * 
+ * @param {string} type - Type of education univeristy or school
+ * 
+ * @requestBody {Object}
+ * @property {string} program
+ * @property {string} course
+ * @property {string} branch
+ * @property {number} semester
+ * @property {array{string}} subjects
+ * 
+ * @return {object} A Response having status code and a message
+ */
+router.post("/create-education", createEducation);
+
+/**
+ * @description Get list of education for universities or schools
+ * @route GET /get-education-list
+ * @access Private
+ * 
+ * @param {string} type - Type of education univeristy or school
+ * @param {number} page - Page number for pagination
+ * @param {number} limit - The number of items per page in the list
+ * 
+ * @return {object} Response having status code, message and data (number of items per page in the list)
+ */
+router.get("/get-education-list", getEducation);
+
+// router.get("/education-type", getOptions);
 
 module.exports = router;
