f1fdff64c9bb7a0548b068de94f48dab886ebd76
Subrat kumar jena <subratj611@gmail.com>
2023-12-15 00:26:04 +0530

    feat(user-management): edit api for updating a user from user management list

--- controllers/admin/assessmentCoordinator.controller.js
+++ controllers/admin/assessmentCoordinator.controller.js
@@ -14,7 +14,35 @@ async function getAssessmentCoordinator(req, res) {
   getUsersFromDB(req, res, AssessmentCoordinator);
 }
 
+async function updateAssessmentCoordinator(req, res) {
+  const { userId, ...updateData } = req.body;
+
+  if (!userId) return res.status(401).json({ message: "user id not provided" });
+
+  try {
+    const user = await AssessmentCoordinator.findById(userId);
+
+    if (!user)
+      return res.status(401).json({ message: "user id does not exist" });
+
+    const updateUserInfo = await AssessmentCoordinator.findByIdAndUpdate(
+      userId,
+      updateData,
+      { new: true, select: "-password" }
+    );
+
+    res.status(200).json({
+      success: true,
+      message: "user updated successfully'",
+      data: updateUserInfo,
+    });
+  } catch (err) {
+    return res.status(500).json({ message: "Internal Server Error!" });
+  }
+}
+
 module.exports = {
   addAssessmentCoordinator,
   getAssessmentCoordinator,
+  updateAssessmentCoordinator,
 };
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -6,6 +6,7 @@ const {
 const {
   addAssessmentCoordinator,
   getAssessmentCoordinator,
+  updateAssessmentCoordinator,
 } = require("../controllers/admin/assessmentCoordinator.controller");
 const optionsController = require("../controllers/admin/educationType.controller");
 const {
@@ -47,7 +48,7 @@ router.post("/login", adminLogin);
  * @description Create Assessment Coordinator
  * @route POST /create-assessment-coordinator
  * @access Private
- * 
+ *
  * @requestBody {Object}
  * @property {string} first_name
  * @property {string} last_name
@@ -56,7 +57,7 @@ router.post("/login", adminLogin);
  * @property {string} profile_pic
  * @property {array{string}} subject_access
  * @property {string} user_type
- * 
+ *
  * @return {object} A Response having status code and a message
  */
 
@@ -66,30 +67,41 @@ router.post("/create-assessment-coordinator", addAssessmentCoordinator);
  * @description Get list assessment coordinator
  * @route GET /get-assessment-coordinator
  * @access Private
- * 
+ *
  * @param {string} type - Type of assessment coordinator
  * @param {number} page - Page number for pagination
  * @param {number} limit - The number of items per page in the list
- * 
+ *
  * @return {object} Response having status code, message and data (number of items per page in the list)
  */
 router.get("/get-assessment-coordinator", getAssessmentCoordinator);
 
+/**
+ * @description Edit assessment coordinator
+ * @route POST /edit-assessment-coordinator
+ * @access Private
+ *
+ * @requestBody {Object}
+ * @property {string} userId
+ *
+ * @returns {object} Response having status code, message and updated user data.
+ */
+router.post("/edit-assessment-coordinator", updateAssessmentCoordinator);
 
 /**
  * @description Create an education for univeristy or school
  * @route POST /create-education
  * @access Private
- * 
+ *
  * @param {string} type - Type of education univeristy or school
- * 
+ *
  * @requestBody {Object}
  * @property {string} program
  * @property {string} course
  * @property {string} branch
  * @property {number} semester
  * @property {array{string}} subjects
- * 
+ *
  * @return {object} A Response having status code and a message
  */
 router.post("/create-education", createEducation);
@@ -98,11 +110,11 @@ router.post("/create-education", createEducation);
  * @description Get list of education for universities or schools
  * @route GET /get-education-list
  * @access Private
- * 
+ *
  * @param {string} type - Type of education univeristy or school
  * @param {number} page - Page number for pagination
  * @param {number} limit - The number of items per page in the list
- * 
+ *
  * @return {object} Response having status code, message and data (number of items per page in the list)
  */
 router.get("/get-education-list", getEducation);
