353bb098f04b1a6fe4359d024073d76d4a439804
Subrat kumar jena <subratj611@gmail.com>
2023-12-17 21:22:23 +0530

    create a custom api error method

---
+++ utils/apiError.js
@@ -0,0 +1,25 @@
+const defaultErrorMessage = require("./index");
+
+class ApiError extends Error {
+  constructor(
+    statusCode,
+    message = defaultErrorMessage,
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.success = false;
+    this.data = null;
+    this.message = message;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+module.exports = ApiError;
