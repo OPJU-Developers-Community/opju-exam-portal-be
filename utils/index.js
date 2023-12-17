f98e90626511c8fcefa221cd41c3a69698470994
Subrat kumar jena <subratj611@gmail.com>
2023-12-17 21:22:35 +0530

    set default error message

--- utils/index.js
+++ utils/index.js
@@ -1,13 +1,1 @@
-function excludeProperty(object, propertyToExclude) {
-  const newObject = {};
-  for (const key in object) {
-    if (key !== propertyToExclude) {
-      newObject[key] = object[key];
-    }
-  } 
-  return newObject;
-}
-
-module.exports = {
-  excludeProperty,
-};
+module.exports.defaultErrorMessage = "An unknown error occurred";
