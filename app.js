e0aedcf648b0003bd0a954ac1ec33101b303b7ab
Subrat kumar jena <subratj611@gmail.com>
2023-12-17 19:07:28 +0530

    add urlencoded middleware

--- app.js
+++ app.js
@@ -15,8 +15,9 @@ const DBConnect = require("./config/mongodb");
 const app = express();
 DBConnect();
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/v1/admin", adminRouter);
