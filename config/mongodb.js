d3c36bbc794a97edcd3f40b521fbab88494fedf5
Subrat kumar jena <subratj611@gmail.com>
2023-12-21 17:32:30 +0530

    minor fix

--- config/mongodb.js
+++ config/mongodb.js
@@ -4,7 +4,7 @@ async function DBConnect() {
   try {
     const connectionInstance = await mongoose.connect(
       process.env.ENVIRONMENT === "dev"
-        ? "mongodb://localhost:27017"
+        ? `mongodb://localhost:27017/${process.env.DB_NAME}`
         : `${process.env.MONGODB_URI}/${process.env.DB_NAME}`
     );
     console.log(`DB CONNECTED, HOST: ${connectionInstance.connection.host}`);
